7470b76bc1f89b882814c111534a495d52de0a45
kimeberz <kimeberz@gmail.com>
2015-03-02 20:31:42 -0800

    mo js homework

---
+++ 2-25/divided_times/js/main2.js
@@ -0,0 +1,141 @@
+$(document).on("click", "li.international", function(event){
+	event.preventDefault();
+	$("#international-drop").show();
+	$("#politics-drop").hide();
+	$("#business-drop").hide();
+	$("#technology-drop").hide();
+	$("#culture-drop").hide();
+	$("#blogs-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+		$("#slide-down").slideUp();
+		$(this).removeClass("active");
+	} else {
+		$("#slide-down").slideDown();
+		$("#primary-nav li").removeClass("active");
+		$(this).addClass("active");
+	}
+});
+
+$(document).on("click", "li.politics", function(event){
+	event.preventDefault();
+	$("#politics-drop").show();
+	$("#international-drop").hide();
+	$("#business-drop").hide();
+	$("#technology-drop").hide();
+	$("#culture-drop").hide();
+	$("#blogs-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+		$("#slide-down").slideUp();
+		$(this).removeClass("active");
+	} else {
+		$("#slide-down").slideDown();
+		$("#primary-nav li").removeClass("active");
+		$(this).addClass("active");
+	}
+});
+
+$(document).on("click", "li.business", function(event){
+	event.preventDefault();
+	$("#business-drop").show();
+	$("#international-drop").hide();
+	$("#politics-drop").hide();
+	$("#technology-drop").hide();
+	$("#culture-drop").hide();
+	$("#blogs-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+			$("#slide-down").slideUp();
+			$(this).removeClass("active");
+		} else {
+			$("#slide-down").slideDown();
+			$("#primary-nav li").removeClass("active");
+			$(this).addClass("active");
+		}
+});
+
+$(document).on("click", "li.technology", function(event){
+	event.preventDefault();
+	$("#technology-drop").show();
+	$("#international-drop").hide();
+	$("#politics-drop").hide();
+	$("#business-drop").hide();
+	$("#culture-drop").hide();
+	$("#blogs-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+			$("#slide-down").slideUp();
+			$(this).removeClass("active");
+		} else {
+			$("#slide-down").slideDown();
+			$("#primary-nav li").removeClass("active");
+			$(this).addClass("active");
+		}
+});
+
+$(document).on("click", "li.culture", function(event){
+	event.preventDefault();
+	$("#culture-drop").show();
+	$("#international-drop").hide();
+	$("#politics-drop").hide();
+	$("#business-drop").hide();
+	$("#technology-drop").hide();
+	$("#blogs-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+			$("#slide-down").slideUp();
+			$(this).removeClass("active");
+		} else {
+			$("#slide-down").slideDown();
+			$("#primary-nav li").removeClass("active");
+			$(this).addClass("active");
+		}
+});
+
+$(document).on("click", "li.blogs", function(event){
+	event.preventDefault();
+	$("#blogs-drop").show();
+	$("#international-drop").hide();
+	$("#politics-drop").hide();
+	$("#business-drop").hide();
+	$("#technology-drop").hide();
+	$("#culture-drop").hide();
+	
+	if ($(this).hasClass("active")) {
+			$("#slide-down").slideUp();
+			$(this).removeClass("active");
+		} else {
+			$("#slide-down").slideDown();
+			$("#primary-nav li").removeClass("active");
+			$(this).addClass("active");
+		}
+});
+
+
+
+// Event preventDefault (as written above) will remove the anchor tag behavior
+// $(document).on("click", "li.international", function(){
+// 	$("#politics-drop").hide();
+// 	$("#business-drop").hide();
+// 	$("#technology-drop").hide();
+// 	$("#culture-drop").hide();
+// 	$("#blogs-drop").hide();
+// 	$("#slide-down").slideToggle();
+// });
+
+
+
+
+// $(document).on("click", "li.international", function(){
+// 	("#slide-down").slideDown( "slow", function(){
+// 	});
+// });
+
+
+
+// $("international").click(function() {
+// 	$("#international-drop").slideDown( "slow", function() {
+
+// 	});
+// });
